dd13b3d283565f72f38d696b235be4e2ad2c2845
Tamal Saha <tamal@appscode.com>
2018-07-20 18:00:15 -0700

    print vars

--- appcred.py
+++ appcred.py
@@ -6,22 +6,32 @@ from keystoneclient.v3 import client
 from pprint import pprint
 import os
 
-AUTH_URL = os.getenv('OS_AUTH_URL')
-USERNAME = os.getenv('OS_USERNAME')
-USER_DOMAIN_NAME = os.getenv('OS_USER_DOMAIN_NAME')
-PASSWD = os.getenv('OS_PASSWORD')
-PROJECT_ID = os.getenv('OS_PROJECT_ID')
-PROJECT_NAME = os.getenv('OS_PROJECT_NAME')
+def main():
+	AUTH_URL = os.getenv('OS_AUTH_URL')
+	USERNAME = os.getenv('OS_USERNAME')
+	USER_DOMAIN_NAME = os.getenv('OS_USER_DOMAIN_NAME')
+	PASSWD = os.getenv('OS_PASSWORD')
+	PROJECT_ID = os.getenv('OS_PROJECT_ID')
+	PROJECT_NAME = os.getenv('OS_PROJECT_NAME')
 
-auth = v3.Password(auth_url=AUTH_URL,
-	username=USERNAME,
-	user_domain_name=USER_DOMAIN_NAME,
-	password=PASSWD,
-	project_id=PROJECT_ID,
-	project_name=PROJECT_NAME)
+	pprint('AUTH_URL = ' + AUTH_URL)
+	pprint('USERNAME = ' + USERNAME)
+	pprint('USER_DOMAIN_NAME = ' + USER_DOMAIN_NAME)
+	pprint('PASSWD = ' + PASSWD)
+	pprint('PROJECT_ID = ' + PROJECT_ID)
+	pprint('PROJECT_NAME = ' + PROJECT_NAME)
 
-sess = session.Session(auth=auth)
-keystone = client.Client(session=sess)
-app_cred = keystone.application_credentials.create(
-	name='kubernetes').to_dict()
-pprint(app_cred)
+	auth = v3.Password(auth_url=AUTH_URL,
+		username=USERNAME,
+		user_domain_name=USER_DOMAIN_NAME,
+		password=PASSWD,
+		project_id=PROJECT_ID,
+		project_name=PROJECT_NAME)
+	sess = session.Session(auth=auth)
+	keystone = client.Client(session=sess)
+	app_cred = keystone.application_credentials.create(
+		name='kubernetes')
+	pprint(app_cred.to_dict())
+
+if __name__ == "__main__":
+	main()
